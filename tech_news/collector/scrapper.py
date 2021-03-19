942f236c985b2612776d23df57054563b1a621c7
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-03-19 12:19:00 -0300

    Subersão 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,11 @@
+import requests
+import time
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
+        response = requests.get(url, timeout)
+        if response.status_code != 200:
+            return ''
+        return response
 
 
 def scrape(fetcher, pages=1):

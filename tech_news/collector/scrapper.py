cbe0ea858ce2040bf87816343a070aec0be969a1
Lucascv <lucascv333@gmail.com>
2021-03-19 15:30:25 -0300

    adicionando timeout e delay na funcao

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,11 +1,13 @@
 import requests
+from time import sleep
 
 
 def fetch_content(url, timeout=3, delay=0.5):
     try:
-        response = requests.get('https://www.tecmundo.com.br/novidades')
+        response = requests.get(url, timeout=timeout)
         if (response.status_code != 200):
             return ''
+        sleep(delay)
         return response.text
     except OSError:
         print('Deu algum erro')

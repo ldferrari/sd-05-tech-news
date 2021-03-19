f277e2ed195a3ff19301aa44391cd9aad118a0ee
ferrari <luisdiasferrari@gmail.com>
2021-03-19 16:36:53 -0300

    requirement 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,19 @@
-def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+import requests
+import time
 
+def fetch_content(url, timeout=3, delay=0.5):
+    try:
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+    except OSError:
+        return ""
+    else:
+        return response.text
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(fetcher)
+    except OSError:
+        return ""
+    else:
+        return response.text

3af732e6a27b23930107d6c1aa03ab619ab6d55f
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-03-19 15:09:41 -0300

    scrapper > getch_content

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,12 +1,28 @@
 import requests
-import time
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
-        response = requests.get(url, timeout)
+    try:
+        response = requests.get(url, timeout=timeout)
+    except:
+        print("deu ruim mano")
+        return ""
+    else:
         if response.status_code != 200:
             return ''
-        return response
+        sleep(delay)
+        return response.text
+     
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
+
+
+if __name__ == '__main__':
+    
+    url = "https://httpbin.org/delay/10"
+    print(fetch_content(url))
+

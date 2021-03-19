2711ee637f2fcbaea3de90cafadad13cf61ac19b
Lucascv <lucascv333@gmail.com>
2021-03-19 15:24:19 -0300

    comecando projeto

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,14 @@
+import requests
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get('https://www.tecmundo.com.br/novidades')
+        if (response.status_code != 200):
+            return ''
+        return response.text
+    except OSError:
+        print('Deu algum erro')
 
 
 def scrape(fetcher, pages=1):

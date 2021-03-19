f51846396cfbad443fe1687d51938bf9c0d0dfc5
ferrari <luisdiasferrari@gmail.com>
2021-03-19 16:58:54 -0300

    requirement 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,11 +1,11 @@
 import requests
-import time
+from time import sleep
 
 
 def fetch_content(url, timeout=3, delay=0.5):
     try:
         response = requests.get(url, timeout=timeout)
-        time.sleep(delay)
+        sleep(delay)
     except OSError:
         return ""
     else:
@@ -14,7 +14,8 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 def scrape(fetcher, pages=1):
     try:
-        response = requests.get(fetcher)
+        response = requests.get(url, timeout=timeout)
+        sleep(delay)
     except OSError:
         return ""
     else:

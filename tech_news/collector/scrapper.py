b3dab2fc50a41604887ec294cc31cd6d15c8ce06
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-03-19 18:05:51 -0300

    quase acabando

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import pytest
+import re
 from parsel import Selector
 
 
@@ -10,16 +11,16 @@ def fetch_content(url, timeout=3, delay=0.5):
         response = requests.get(url, timeout=timeout)
 
     except requests.ReadTimeout:
-        print("readTImeout")
+        # print("readTImeout")
         return ""
 
     if response.status_code and response.status_code == 200:
-        print(response)
+        # print(response)
         time.sleep(delay)
         return response.text
 
     else:
-        return ''
+        return ""
 
 
 # fetch_content("https://app.betrybe.com/")
@@ -28,8 +29,39 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 URL_BASE = "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm"
 
+
 def scrape(fetcher, pages=1):
     response_text = fetcher(URL_BASE)
     selector = Selector(response_text)
 
-    title =
+    title = selector.css("#js-article-title ::text").get()
+    print(title)
+
+    timestamp = selector.css("#js-article-date ::attr(datetime)").get()
+    print(timestamp)
+
+    author = selector.css(".tec--author__info__link ::text").get()
+    print(author)
+
+    shares_count_dirty = selector.css(".tec--toolbar__item ::text").getall()[3]
+    shares_count = [int(i) for i in shares_count_dirty.split() if i.isdigit()][
+        0
+    ]
+    print(shares_count)
+
+    comments_count_dirty = selector.css(".tec--toolbar__item ::text").getall()[
+        8
+    ]
+    comments_count = [
+        int(i) for i in comments_count_dirty.split() if i.isdigit()
+    ][0]
+    print(comments_count)
+
+    # selector.xpath('//div[@id="p402_premium"]/p/text()').get()
+    #  selector.xpath('//div[@id="p402_premium"]/p/text()').get()
+    summary = selector.css(".p402_premium > p *::text").getall()
+
+    print(summary)
+
+
+scrape(fetcher=fetch_content, pages=2)

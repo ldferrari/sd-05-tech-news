f3c95af7739e0cd3a8cd201d59438d9d22964f47
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-03-19 16:53:00 -0300

    tendi nothing

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -4,14 +4,15 @@ import time
 
 def fetch_content(url, timeout=3, delay=0.5):
     try:
-        response = requests.get(url, timeout=3)
+        response = requests.get(url, timeout=timeout)
         time.sleep(delay)
-    except requests.ReadTimeout:
-        response = requests.get(url, timeout=3)
+    except requests.Timeout:
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
     finally:
         if (response.status_code != 200):
             return ''
-    return response
+    return response.text
 
 
 def scrape(fetcher, pages=1):

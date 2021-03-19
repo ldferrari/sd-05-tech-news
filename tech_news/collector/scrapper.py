9b88a90c810380f29c746468e731ef934a6246bb
Christian Duessel <duessel@gmail.com>
2021-03-19 18:24:06 -0300

    req1?

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,19 @@
+import requests
+# from parsel import Selector
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+    except requests.ReadTimeout:
+        # ?response.status_code == '200':
+        return ""
+    else:
+        # return Selector(text=response.text)
+        return response.text
+    finally:
+        time.sleep(delay)
 
 
 def scrape(fetcher, pages=1):

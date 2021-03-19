2b2003ff626428e160f7f037e5d5345cc975d616
pauloricardoz <trybe.przt@gmail.com>
2021-03-19 17:38:42 -0300

    req1 first try

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,15 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+    except requests.exceptions.ReadTimeout:
+        return ""
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):

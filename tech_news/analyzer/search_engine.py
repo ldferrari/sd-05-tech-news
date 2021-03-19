d4ae79204575ee6ddc5bc648df073a06ee1edfb5
Felipe Vieira <flpvieira86@gmail.com>
2021-03-19 16:44:48 -0300

    search_by_source ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -2,8 +2,11 @@ from tech_news import database
 import datetime
 
 
+REGEX = "$regex"
+
+
 def search_by_title(title):
-    news = database.search_news({"title": {"$regex": title, "$options": "-i"}})
+    news = database.search_news({"title": {REGEX: title, "$options": "-i"}})
     if len(news) == 0:
         return news
 
@@ -14,7 +17,7 @@ def search_by_date(date):
     try:
         new_date = datetime.datetime.strptime(date, "%Y-%m-%d")
         print(new_date)
-        news = database.search_news({"timestamp": {"$regex": date}})
+        news = database.search_news({"timestamp": {REGEX: date}})
     except ValueError:
         raise ValueError("Data inválida")
     else:
@@ -24,7 +27,12 @@ def search_by_date(date):
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = database.search_news({"sources": {REGEX: source, "$options": "-i"}})
+
+    if len(news) == 0:
+        return news
+
+    return [(news[0]["title"], news[0]["url"])]
 
 
 def search_by_category(category):

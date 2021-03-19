7b24446ac0ac039a65331aa8dc39abb5cd0cc531
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-03-19 16:19:03 -0300

    linter :(

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 
+
 def fetch_content(url, timeout=3, delay=0.5):
     try:
         response = requests.get(url, timeout=3)
@@ -11,10 +12,7 @@ def fetch_content(url, timeout=3, delay=0.5):
         if (response.status_code != 200):
             return ''
     return response
-        
-
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
-

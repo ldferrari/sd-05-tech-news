0bf961145172637cce94cab58f6a0d5596af2050
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-03-19 16:13:04 -0300

    requisito 1 completo

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(delay)
+    except requests.ReadTimeout:
+        response = requests.get(url, timeout=3)
+    finally:
+        if (response.status_code != 200):
+            return ''
+    return response
+        
+
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
+

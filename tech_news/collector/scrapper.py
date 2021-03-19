ffc54a6fbc4db8603eb46772ef7412bd728d99d9
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-03-19 17:54:03 -0300

    corrigido

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,18 +1,17 @@
 import requests
-import time
+from time import sleep
 
 
 def fetch_content(url, timeout=3, delay=0.5):
     try:
         response = requests.get(url, timeout=timeout)
-        time.sleep(delay)
     except requests.Timeout:
-        response = requests.get(url, timeout=timeout)
-        time.sleep(delay)
-    finally:
+        return ''
+    else:
         if (response.status_code != 200):
             return ''
-    return response.text
+        sleep(delay)
+        return response.text
 
 
 def scrape(fetcher, pages=1):

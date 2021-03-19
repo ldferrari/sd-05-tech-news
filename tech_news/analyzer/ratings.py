86c24a931ab7c507f04dada1da39f20ff535eec6
Felipe Vieira <flpvieira86@gmail.com>
2021-03-19 16:56:26 -0300

    top_5_categories ok

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,9 +1,10 @@
 import pymongo
 from tech_news.database import db
+from bson.son import SON
 
 
 def top_5_news():
-    data = (
+    DATA = (
         db.news.find({}).sort([
                 ("shares_count", pymongo.DESCENDING),
                 ("comments_count", pymongo.DESCENDING),
@@ -12,10 +13,20 @@ def top_5_news():
         ).limit(5)
     )
     news_data = []
-    for i in data:
+    for i in DATA:
         news_data.append((i["title"], i["url"]))
     return news_data
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    DATA = db.news.aggregate([
+        {"$unwind": "$categories"},
+        {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+        {"$sort": SON([("count", 1), ("_id", 1)])},
+        {"$limit": 5},
+    ])
+    news_data = []
+    for i in DATA:
+        news_data.append((i["_id"]))
+
+    return news_data

ececb01700cfc86b2bf891fb008b97e4fbd07124
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-03-19 18:52:38 -0300

    req1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,21 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+    except requests.Timeout:
+        return ''
+    else:
+        if (response.status_code != 200):
+            return ''
+        time.sleep(delay)
+        return response.text
+
+# https://requests.readthedocs.io/en/master/user/advanced/#timeouts
+# https://realpython.com/python-sleep/
 
 
 def scrape(fetcher, pages=1):

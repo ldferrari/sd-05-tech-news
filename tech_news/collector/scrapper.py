49a666deba2a180d92f83601e574cc85c43c1ae8
luiz-avelar <luiz.avelar25@gmail.com>
2021-03-19 18:50:14 -0300

    Requirement 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,20 @@
+# Referências:
+# https://stackoverflow.com/questions/19342111/get-http-error-code-from-requests-exceptions-httperror
+
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+    except (requests.ReadTimeout, requests.HTTPError):
+        return ""
+    else:
+        return response.text
+    finally:
+        sleep(delay)
 
 
 def scrape(fetcher, pages=1):

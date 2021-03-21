aeef3cb29747d1884a65619862644d7ff3a59ce6
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-03-21 16:01:50 -0300

    Subversão 5

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,18 @@
+from tech_news.model import tech_news_model
+
+cursor = tech_news_model.find_cursor
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    PARAMS = {"title": {"$regex": f".*{title}.*", "$options": "i"}}
+    PROJECTION = {"_id": 0, "title": 1, "url": 1}
+    search = cursor(params=PARAMS, projection=PROJECTION)
+    if len(search) == 0:
+        return []
+    result = [tuple(reversed(item.values())) for item in search]
+    print(result)
+    return result
 
 
 def search_by_date(date):

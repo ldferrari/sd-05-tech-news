36c602a895beae10c2bc72b8a4c4ddc0fdea63d0
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-03-21 17:03:22 -0300

    Subversão 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,22 +1,41 @@
+from datetime import date as dt
 from tech_news.model import tech_news_model
 
 cursor = tech_news_model.find_cursor
+PROJECTION = {"_id": False, "title": True, "url": True}
+
+
+def validate_date(date):
+    year, month, day = date.split("-")
+    try:
+        assert len(year) == 4
+        assert len(month) == 2
+        assert len(day) == 2
+    except AssertionError:
+        raise ValueError("Data inválida")
+    else:
+        return date
 
 
 def search_by_title(title):
     """Seu código deve vir aqui"""
     PARAMS = {"title": {"$regex": f".*{title}.*", "$options": "i"}}
-    PROJECTION = {"_id": 0, "title": 1, "url": 1}
     search = cursor(params=PARAMS, projection=PROJECTION)
     if len(search) == 0:
         return []
     result = [tuple(reversed(item.values())) for item in search]
-    print(result)
     return result
 
 
-def search_by_date(date):
+def search_by_date(date, validator=validate_date):
     """Seu código deve vir aqui"""
+    valid_date = validator(date)
+    PARAMS = {"timestamp": {"$regex": f"^{valid_date}"}}
+    search = cursor(params=PARAMS, projection=PROJECTION)
+    if len(search) == 0:
+        return []
+    result = [tuple(reversed(item.values())) for item in search]
+    return result
 
 
 def search_by_source(source):

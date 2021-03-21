73f5e858a71ce4a7862adc9c752a655b94742773
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-03-21 15:59:25 -0300

    Cria um model para trabalhar o banco

---
+++ tech_news/model/tech_news_model.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+from decouple import config
+
+DB_HOST = config("DB_HOST", default="localhost")
+DB_PORT = config("DB_PORT", default="27017")
+
+
+def connection(base="tech_news"):
+    """
+    DEFINITIONS:
+        base: Database to connect, default is 'tech_news'.
+    """
+    with MongoClient(host=DB_HOST, port=int(DB_PORT)) as client:
+        return client[base]
+
+
+def find_cursor(
+    params={}, projection={"_id": 0}, collection="news", conn=connection
+):
+    """
+    DEFINITIONS:
+        params: type DICT, Parâmetros de busca a ser realizada no banco.
+        projection: type DICT, default {"_id": 0}
+        collection: type STRING, coleção de busca, default 'news'
+        conn: type FUNCTION, Conexão a ser usada, default connection
+    """
+    db = conn()
+    result = list(db[collection].find(params, projection))
+    return result

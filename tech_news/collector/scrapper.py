531b407ba0b96360da2187cb2368e1e4c072c310
Rafael Assad <rafaassadeng@gmail.com>
2021-03-22 16:31:48 -0300

    def max hp

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,11 @@
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
-
+    try:
+        response = requests.get(url, timeout=timeout)
+    except requests.Timeout:
+                return ''
+            time.sleeep(delay)
+            return response.text
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""

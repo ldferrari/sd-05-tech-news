c27aecdb6fe86ec728e0bec7c6d048bfbfd59823
Felipe Vieira <flpvieira86@gmail.com>
2021-03-22 20:29:10 -0300

    analyzer_menu ok

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -3,6 +3,11 @@ from tech_news.collector.importer import csv_importer
 from tech_news.collector.exporter import csv_exporter
 from tech_news.collector.scrapper import scrape, fetch_content
 from tech_news.database import create_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_source,
+)
 
 
 def collector_menu():
@@ -32,4 +37,27 @@ def collector_menu():
 
 
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    INPUT = input(
+        "Selecione uma das opções a seguir:\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por fonte;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair.\n"
+    )
+
+    if INPUT == "1":
+        TITLE = input("Digite o título: ")
+        search_by_title(TITLE)
+    elif INPUT == "2":
+        DATE = input("Digite a data no formato aaaa-mm-dd: ")
+        search_by_date(DATE)
+    elif INPUT == "3":
+        SOURCE = input("Digite a fonte: ")
+        search_by_source(SOURCE)
+    elif INPUT == "7":
+        return print("Encerrando script")
+    else:
+        return print("Opção inválida", file=sys.stderr)

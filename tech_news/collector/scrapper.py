b5fa8e40be40f98757fc3cd00bc76bf69f33fcf0
llsxavier <leonardoxw@gmail.com>
2021-03-22 09:49:36 -0300

    nunca mais colocarei "." entre um "status" e o seu "code", rs.

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,17 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return ''
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
 
 
 def scrape(fetcher, pages=1):

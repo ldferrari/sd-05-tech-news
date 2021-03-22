a556dd9991109554db2f55b69498854111fa014e
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-03-22 14:36:08 -0300

    falta passar 2 de 5 para o scrapper

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -33,57 +33,32 @@ def scrape(fetcher, pages=1):
         selector = Selector(text=response_text)
 
         for url in selector.css(".tec--list__item h3 a::attr(href)").getall():
-            # print(page)
-            # print(url)
             selector_url = Selector(text=fetcher(url))
-            # print(selector_url)
             title = selector_url.css(
                 ".tec--article__header__title::text"
             ).get()
-            # print(title)
 
             timestamp = selector_url.css(
                 "#js-article-date ::attr(datetime)"
             ).get()
-            # print(timestamp)
 
             author = selector_url.css(".tec--author__info__link ::text").get()
-            # # print(author)
 
             shares_count = int(
-              selector_url.css("div.tec--toolbar__item:nth-child(1)::text").re_first(r"[0-9]+")
+                selector_url.css(
+                    "div.tec--toolbar__item:nth-child(1)::text"
+                ).re_first(r"[0-9]+")
             )
-            # print('shares_count')
-            # print(shares_count)
-            # shares_count =
-            # print(shares_count)
-            # shares_count = [
-            #     int(i) for i in shares_count_dirty.split() if i.isdigit()
-            # ]
-            # # print(shares_count)
 
-            comments_count = selector_url.css(
-                "#js-comments-btn::text"
-            ).re_first(r"[0-9]+")
-            # print('comments_count')
-            # print(comments_count)
-            # comments_count = [
-            #     int(i) for i in comments_count_dirty.split() if i.isdigit()
-            # ]
-            # # print(comments_count)
-
-            summary = selector_url.css(".p402_premium > p *::text").getall()
-            # summary = "".join([str(elem) for elem in summary_list])
-            # # print(summary)
-
-            sources = (selector_url.css(".z--mb-16 a::text").getall(),)
-            # # print(sources)
-
-            categories = (
-                selector_url.css("a.tec--badge--primary::text").getall(),
+            comments_count = int(
+                selector_url.css("#js-comments-btn::text").re_first(r"[0-9]+")
             )
-            # categories = [elem.strip() for elem in categories_dirty]
-            # # print(categories)
+
+            summary = selector_url.css(".tec--article__body *::text").get()
+
+            sources = selector_url.css(".z--mb-16 a::text").getall()
+
+            categories = selector_url.css("a.tec--badge--primary::text").getall()
 
             list_news.append(
                 {

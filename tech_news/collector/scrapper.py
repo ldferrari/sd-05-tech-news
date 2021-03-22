474f32f45b3d4d9f880b821396f8a9450af69f66
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-03-22 14:05:10 -0300

    falta passar 3 dos 7 dos primeiros 2 requisitos

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,7 +1,5 @@
 import requests
 import time
-import pytest
-import re
 from parsel import Selector
 
 URL_BASE = "https://www.tecmundo.com.br/novidades"
@@ -26,53 +24,64 @@ def fetch_content(url, timeout=3, delay=0.5):
         return ""
 
 
-def scrape(fetcher, pages=1): 
+def scrape(fetcher, pages=1):
     list_news = []
-    page = 1 
+    page = 1
 
     while page <= pages:
         response_text = fetcher(URL_BASE + "?page={page}")
-        # print(response_text)
         selector = Selector(text=response_text)
-        # porra = selector.css(".tec--list__item h3 a::attr(href)").getall()
-        # print(porra) 
 
         for url in selector.css(".tec--list__item h3 a::attr(href)").getall():
-            print(page)
+            # print(page)
             # print(url)
             selector_url = Selector(text=fetcher(url))
             # print(selector_url)
-            title = selector_url.css(".tec--article__header__title::text").get()
+            title = selector_url.css(
+                ".tec--article__header__title::text"
+            ).get()
             # print(title)
 
-            # timestamp = selector_url.css("#js-article-date ::attr(datetime)").get().strip()
-            # # print(timestamp)
+            timestamp = selector_url.css(
+                "#js-article-date ::attr(datetime)"
+            ).get()
+            # print(timestamp)
 
-            # author = selector_url.css(".tec--author__info__link ::text").get().strip()
+            author = selector_url.css(".tec--author__info__link ::text").get()
             # # print(author)
 
-            # shares_count_dirty = selector_url.css(".tec--toolbar__item ::text").getall()[3]
-            # shares_count = [int(i) for i in shares_count_dirty.split() if i.isdigit()][
-            #     0
+            shares_count = int(
+              selector_url.css("div.tec--toolbar__item:nth-child(1)::text").re_first(r"[0-9]+")
+            )
+            # print('shares_count')
+            # print(shares_count)
+            # shares_count =
+            # print(shares_count)
+            # shares_count = [
+            #     int(i) for i in shares_count_dirty.split() if i.isdigit()
             # ]
             # # print(shares_count)
 
-            # comments_count_dirty = selector_url.css(".tec--toolbar__item ::text").getall()[
-            #     8
-            # ]
+            comments_count = selector_url.css(
+                "#js-comments-btn::text"
+            ).re_first(r"[0-9]+")
+            # print('comments_count')
+            # print(comments_count)
             # comments_count = [
             #     int(i) for i in comments_count_dirty.split() if i.isdigit()
-            # ][0]
+            # ]
             # # print(comments_count)
 
-            # summary_list = selector_url.css(".p402_premium > p *::text").getall()
+            summary = selector_url.css(".p402_premium > p *::text").getall()
             # summary = "".join([str(elem) for elem in summary_list])
             # # print(summary)
 
-            # sources = selector_url.css(".tec--badge *::text").get().strip()
+            sources = (selector_url.css(".z--mb-16 a::text").getall(),)
             # # print(sources)
 
-            # categories_dirty = selector_url.css("#js-categories > a *::text").getall()
+            categories = (
+                selector_url.css("a.tec--badge--primary::text").getall(),
+            )
             # categories = [elem.strip() for elem in categories_dirty]
             # # print(categories)
 
@@ -80,17 +89,17 @@ def scrape(fetcher, pages=1):
                 {
                     "url": url,
                     "title": title,
-                    # "timestamp": timestamp,
-                    # "writer": author,
-                    # "shares_count": shares_count,
-                    # "comments_count": comments_count,
-                    # "summary": summary,
-                    # "sources": [sources],
-                    # "categories": categories,
+                    "timestamp": timestamp,
+                    "writer": author,
+                    "shares_count": shares_count,
+                    "comments_count": comments_count,
+                    "summary": summary,
+                    "sources": [sources],
+                    "categories": categories,
                 }
             )
         page += 1
     return list_news
-    
 
-print(scrape(fetcher=fetch_content, pages=2)[1])
+
+# print(scrape(fetcher=fetch_content, pages=2)[1])

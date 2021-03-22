ad83e1b4b0c2cca7c880b08c916ed12221a35995
Christian Duessel <duessel@gmail.com>
2021-03-22 17:30:20 -0300

    req 4 mas falta o retorno montadinho

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,31 @@
+import csv
+# from pymongo import MongoClient
+from database import find_news
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    # DB_HOST = config("DB_HOST", default="localhost")
+    # DB_PORT = config("DB_PORT", default="27017")
+    # with client = MongoClient(host=DB_HOST, port=int(DB_PORT))
+    #    db = client.tech_news
+    try:
+        assert filepath.endswith('.csv')
+    except AssertionError:
+        raise ValueError("Formato invalido")
+    else:
+        with open(filepath, "w") as file:
+            writer = csv.writer(file, delimiter=";")
+            headers = [
+                "url",
+                "title",
+                "timestamp",
+                "writer",
+                "shares_count",
+                "comments_count",
+                "summary",
+                "sources",
+                "categories",
+            ]
+            writer.writerow(headers)
+            news = find_news()
+            writer.writerow(news)

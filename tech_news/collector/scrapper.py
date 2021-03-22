69067229f26fdfdb5e14740e1dbdd20ea11b61a5
Giuliano Peixoto <giulianopeixoto@gmail.com>
2021-03-22 18:38:31 -0300

    req1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,19 @@
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        sleep(delay)
+    except requests.Timeout:
+        return ""
+    else:
+        if (response.status_code != 200):
+            return ""
+    return (response.text)
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
+  

88db2cddbd1ab312c3acbc93a4786eaad8e3b114
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-03-23 18:46:13 -0300

    req6 search by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news import database
+from datetime import datetime
 
 
 def search_by_title(title):
@@ -17,7 +18,21 @@ def search_by_title(title):
 
 
 def search_by_date(date):
+    # 1/ Same logic & expected result format
+    # 2/ But with datetime lib and invalid error
     """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        # (até sem assert, dispara ValueError se for inconsistente)
+        NEWS = database.search_news({"timestamp": {"$regex": date}})
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        if len(NEWS) == 0:
+            return []
+        return [(NEWS[0]["title"], NEWS[0]["url"])]
+
+# https://docs.python.org/3/library/datetime.html
 
 
 def search_by_source(source):
@@ -29,3 +44,6 @@ def search_by_category(category):
 
 
 # https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362
+# Honestidade acadêmica:
+# PR do aluno Felipe Vieira para lembrar da norma de var maiuscula (NEWS)
+# https://github.com/tryber/sd-05-tech-news/pull/17/files

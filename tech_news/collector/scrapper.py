c0904a85758e6f84ea5a6136e2e68383ad2f2de0
Renan <luucadecastro@gmail.com>
2021-03-23 11:51:21 -0300

    linter

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,6 @@
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui!""
+    """Seu código deve vir aqui"""
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui!""
+    """Seu código deve vir aqui"""

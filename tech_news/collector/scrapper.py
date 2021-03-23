dce837b2561f5c87615f1eb9dfa01502b961a9d2
Renan <luucadecastro@gmail.com>
2021-03-23 17:24:27 -0300

    req 1.2

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -5,14 +5,15 @@ from time import sleep
 def fetch_content(url, timeout=3, delay=0.5):
     try:
         response = requests.get(url, timeout=timeout)
-        sleep(delay)
     except requests.RequestException:
         return ''
     else:
         if response.status_code != 200:
             return ''
-        return response.text()
+
+        sleep(delay)
+        return response.text
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    fetcher(url)

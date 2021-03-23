01cbbd17156dc578af1dfb43297a3c9f36a19335
Lucascv <lucascv333@gmail.com>
2021-03-23 17:50:45 -0300

    req3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -3,9 +3,19 @@ import csv
 
 def csv_importer(filepath):
     if filepath.split('.')[1] == 'csv':
-        with open(filepath, 'r') as news:
-            ler = csv.reader(news, delimiter=';')
-            print(ler)
+        try:
+            with open(filepath, 'r') as news:
+                ler = csv.reader(news, delimiter=';')
+                header, *data = ler
+        except FileNotFoundError:
+            raise ValueError(f"Arquivo {filepath} não encontrado")
+        else:
+            output = []
+            lista = {}
+            for index, value in enumerate(header):
+                lista[value] = data[0][index]
 
-
-csv_importer('file_csv.csv')
+            output.append(lista)
+            return output
+    else:
+        raise ValueError("Formato invalido")

4ca086c9808298658d7e9a1c1b683ba3ffbdab32
César Alves <alvescesar@hotmail.com>
2021-03-23 02:25:44 -0300

    Req 5 search_by_title done

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,20 @@
+from tech_news import database
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    search_news = database.search_news(
+        {
+            "title":
+                {
+                    "$regex": title, "$options": "i"
+                }
+        }
+    )
+
+    if len(search_news) == 0:
+        return []
+
+    return [(search_news[0]["title"], search_news[0]["url"])]
 
 
 def search_by_date(date):

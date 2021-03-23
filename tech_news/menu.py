5812299aa252c188bd4763e2c928b6353ee30eb6
Felipe Vieira <flpvieira86@gmail.com>
2021-03-22 21:32:53 -0300

    complexidade reduzida

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -39,7 +39,7 @@ def collector_menu():
 
 
 def analyzer_menu():
-    INPUT = input(
+    INPUT = int(input(
         "Selecione uma das opções a seguir:\n"
         " 1 - Buscar notícias por título;\n"
         " 2 - Buscar notícias por data;\n"
@@ -48,25 +48,29 @@ def analyzer_menu():
         " 5 - Listar top 5 notícias;\n"
         " 6 - Listar top 5 categorias;\n"
         " 7 - Sair.\n"
-    )
+    ))
 
-    if INPUT == "1":
-        TITLE = input("Digite o título: ")
-        search_by_title(TITLE)
-    elif INPUT == "2":
-        DATE = input("Digite a data no formato aaaa-mm-dd: ")
-        search_by_date(DATE)
-    elif INPUT == "3":
-        SOURCE = input("Digite a fonte: ")
-        search_by_source(SOURCE)
-    elif INPUT == "4":
-        SOURCE = input("Digite a categoria: ")
-        search_by_category(SOURCE)
-    elif INPUT == "5":
-        top_5_news()
-    elif INPUT == "6":
-        top_5_categories()
-    elif INPUT == "7":
-        return print("Encerrando script")
+    FUNCTIONS = {
+        1: lambda title: print(search_by_title(title)),
+        2: lambda date: print(search_by_date(date)),
+        3: lambda source: print(search_by_source(source)),
+        4: lambda category: print(search_by_category(category)),
+        5: lambda: print(top_5_news()),
+        6: lambda: print(top_5_categories()),
+        7: lambda: print("Encerrando script"),
+    }
+
+    INPUTS = {
+        1: "Digite o título:",
+        2: "Digite a data no formato aaaa-mm-dd:",
+        3: "Digite a fonte:",
+        4: "Digite a categoria:",
+    }
+
+    if 0 < INPUT < 5:
+        USER_INPUT = input(INPUTS[INPUT])
+        FUNCTIONS[INPUT](USER_INPUT)
+    elif 4 < INPUT < 8:
+        FUNCTIONS[INPUT]()
     else:
-        return print("Opção inválida", file=sys.stderr)
+        print("Opção inválida", file=sys.stderr)

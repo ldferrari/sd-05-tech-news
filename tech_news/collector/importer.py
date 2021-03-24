6691dd66641f967a4f12fcbbc3a6aba3e5b1f8c1
ferrari <luisdiasferrari@gmail.com>
2021-03-24 08:54:13 -0300

    requirement 3 and 4

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -2,12 +2,14 @@ import csv
 
 
 def csv_importer(filepath):
+    results = []    
     if filepath.split('.')[1] != "csv":
         raise ValueError("Formato invalido")
     try:
         with open(filepath) as file:
             reader_file = csv.DictReader(file, delimiter=";")
             for linha in reader_file:
-                return [linha]
+                results.append(linha)
+        return results
     except FileNotFoundError:
         raise ValueError(f'Arquivo {filepath} não encontrado')

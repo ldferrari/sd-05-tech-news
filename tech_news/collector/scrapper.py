bb9e3db339b11055bee5a370333910b646a53238
Christian Duessel <duessel@gmail.com>
2021-03-25 13:57:57 -0300

    req 2 pI

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,5 @@
 import requests
-# from parsel import Selector
+from parsel import Selector
 from time import sleep
 
 
@@ -19,3 +19,17 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
+    result = []
+    for page in range(pages):
+        base_url = "https://www.tecmundo.com.br/novidades"
+        page_url = f"?page={page}"
+        the_url = base_url + page_url
+        selector = Selector(text=fetcher(the_url))
+        for url in selector.css(".tec--list__item h3 a::attr(href)").getall():
+            new_selector = parsel.Selector(text=fetcher(url))
+            title = new_selector.css(
+                ".tec--article__header__title::text"
+                ).get()
+            timestamp = new_selector.css(
+                "#js-article-date ::attr(datetime)"
+            ).get()

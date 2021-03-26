acb92d76b038a891ef7a13ccc072a221db1a7ccd
Christian Duessel <duessel@gmail.com>
2021-03-26 14:00:42 -0300

    comentei estrutura do try except para tirar complexidade e tentar passar pelo flake para o req2

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -8,32 +8,35 @@ def csv_exporter(filepath):
     # DB_PORT = config("DB_PORT", default="27017")
     # with client = MongoClient(host=DB_HOST, port=int(DB_PORT))
     #    db = client.tech_news
-    try:
-        assert filepath.endswith('.csv')
-    except AssertionError:
+    # comentei estrutura do try excerpt para fugir do "too complex" do flake
+    # try:
+    #     assert filepath.endswith('.csv')
+    # except AssertionError:
+    #     raise ValueError("Formato invalido")
+    if not filepath.endswith('.csv'):
         raise ValueError("Formato invalido")
-    else:
-        with open(filepath, "w") as file:
-            headers = [
-                "url",
-                "title",
-                "timestamp",
-                "writer",
-                "shares_count",
-                "comments_count",
-                "summary",
-                "sources",
-                "categories",
-            ]
-            writer = csv.DictWriter(file, delimiter=";", fieldnames=headers)
-            writer.writeheader()
-            news = find_news()
-            for rows in news:
-                for key in rows:
-                    if isinstance(rows[key], list):
-                        rows[key] = ",".join(rows[key])
-            writer.writerows(news)
+    # else:
+    with open(filepath, "w") as file:
+        headers = [
+            "url",
+            "title",
+            "timestamp",
+            "writer",
+            "shares_count",
+            "comments_count",
+            "summary",
+            "sources",
+            "categories",
+        ]
+        writer = csv.DictWriter(file, delimiter=";", fieldnames=headers)
+        writer.writeheader()
+        news = find_news()
+        for rows in news:
+            for key in rows:
+                if isinstance(rows[key], list):
+                    rows[key] = ",".join(rows[key])
+        writer.writerows(news)
 
 # transparencia academica: conversei com colegas que sugeriram os artigos:
 # https://stackoverflow.com/questions/2982023/how-to-write-header-row-with-csv-dictwriter
-# # https://www.w3schools.com/python/ref_func_isinstance.asp
+# https://www.w3schools.com/python/ref_func_isinstance.asp

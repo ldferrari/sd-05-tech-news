caa5dac39b6cf0277b5e04076c7dd9da1fe62cd8
luiz-avelar <luiz.avelar25@gmail.com>
2021-03-27 14:59:33 -0300

    Requirement 5

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,15 @@
+# Referência: https://stackoverflow.com/questions/10610131/
+# checking-if-a-field-contains-a-string
+
+from tech_news import database
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+
+    news = database.search_news({"title": {"$regex": title, "$options": "i"}})
+    print(news)
+    return [(item["title"], item["url"]) for item in news]
 
 
 def search_by_date(date):
@@ -12,3 +22,7 @@ def search_by_source(source):
 
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+if __name__ == "__main__":
+    search_by_title("VAMOSCOMTUDO")

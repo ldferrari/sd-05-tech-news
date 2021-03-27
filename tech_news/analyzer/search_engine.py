a83cbfa72cd1ea7c597bd8273124537c5379dc48
luiz-avelar <luiz.avelar25@gmail.com>
2021-03-27 16:58:07 -0300

    Requirement 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,7 @@
 # Referências:
 # https://stackoverflow.com/questions/10610131/checking-if-a-field-contains-a-string
 # https://stackoverflow.com/questions/16870663/how-do-i-validate-a-date-string-format-in-python/16870699
+# https://stackoverflow.com/questions/10700921/case-insensitive-search-with-in
 
 from tech_news import database
 import datetime
@@ -26,6 +27,10 @@ def search_by_date(date):
 
 def search_by_source(source):
     """Seu código deve vir aqui"""
+    news = database.search_news(
+        {"sources": {"$elemMatch": {"$regex": source, "$options": "i"}}}
+    )
+    return [(item["title"], item["url"]) for item in news]
 
 
 def search_by_category(category):
@@ -34,6 +39,9 @@ def search_by_category(category):
 
 if __name__ == "__main__":
     # search_by_title("VAMOSCOMTUDO")
-    print(search_by_date("2020-11-23"))
-    print(search_by_date("2019-12-12"))
-    print(search_by_date("21-12-1980"))
+    # print(search_by_date("2020-11-23"))
+    # print(search_by_date("2019-12-12"))
+    # print(search_by_date("21-12-1980"))
+    print(search_by_source("ResetEra"))
+    print(search_by_source("fonte_invalida"))
+    print(search_by_source("RESETERA"))

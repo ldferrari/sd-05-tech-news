1ca6f7eb0f1c5e9839c6c719552455988b85be15
ferrari <luisdiasferrari@gmail.com>
2021-03-26 23:10:04 -0300

    requirement 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -26,7 +26,6 @@ def search_by_date(date):
 
 
 def search_by_source(source):
-    print(source)
     search = search_news({"sources": {"$regex": source, "$options": "i"}})
     if search == []:
         return []
@@ -35,4 +34,8 @@ def search_by_source(source):
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    search = search_news({"categories": {"$regex": category, "$options": "i"}})
+    if search == []:
+        return []
+    news = [(search[0]["title"], search[0]["url"])]
+    return news

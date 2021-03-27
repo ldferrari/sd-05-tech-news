b7930c7fc4c4eb45aee9656ec7e6ca31e30ff034
Renan <luucadecastro@gmail.com>
2021-03-27 19:42:14 -0300

    requisito 3

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,27 @@
+from csv import writer
+from tech_news import database
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    if (not filepath.lower().endswith(".csv")):
+        raise Exception("Este arquivo não é formato csv")
+    news = database.find_news()
+    with open(filepath, "w") as file:
+        file_writer = writer(file, delimiter=";", fieldnames=[
+            "url",
+            "title",
+            "timestamp",
+            "writer",
+            "shares_count",
+            "comments_count",
+            "summary",
+            "sources",
+            "categories",
+        ])
+
+        for rows in news:
+            for key in rows:
+                if type(rows[key]) == list:
+                    rows[key] = ",".join(rows[key])
+        file_writer.writeheader()
+        file_writer.writerows(news)

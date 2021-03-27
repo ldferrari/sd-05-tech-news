6b716050922611366747246860c11823973ed3b1
RenataNovais <rnovaiscs@gmail.com>
2021-03-27 18:53:04 -0300

    Ajuste Flake8 - Req 1.

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,15 +1,22 @@
 import requests
 import time
 
+
 def fetch_content(url, timeout=3, delay=0.5):
     """Realiza uma requisição HTTP e retorna conteúdo como resposta."""
-    response = requests.get(url, timeout=timeout)
-    time.sleep(delay)
 
-    if response and response.status_code != 200:
+    try:
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+
+        if response and response.status_code != 200:
+            return ''
+
+        return response.text
+
+    except requests.Timeout:
         return ''
 
-    return response.text
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""

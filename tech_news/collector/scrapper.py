e3713ec33cb6ca6ed20abce47d64dcd493446382
Andrevlc <andrevezi@gmail.com>
2021-03-27 15:00:25 -0300

    Requisite 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,20 @@
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+    except requests.ReadTimeout:
+        return ""
+    else:
+        code_success = 200
+        if response.status_code != code_success:
+            return ""
+        return response.text
+
+    finally:
+        sleep(delay)
 
 
 def scrape(fetcher, pages=1):

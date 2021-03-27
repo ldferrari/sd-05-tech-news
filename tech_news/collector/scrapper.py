7077e153689b64dbd629ff4c53f3ea17e1c4ce2c
RenataNovais <rnovaiscs@gmail.com>
2021-03-27 18:31:17 -0300

    Requisito 1 - requisiçao HTTP.

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,15 @@
-def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+import requests
+import time
 
+def fetch_content(url, timeout=3, delay=0.5):
+    """Realiza uma requisição HTTP e retorna conteúdo como resposta."""
+    response = requests.get(url, timeout=timeout)
+    time.sleep(delay)
+
+    if response and response.status_code != 200:
+        return ''
+
+    return response.text
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""

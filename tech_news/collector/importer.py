9c66ceba18abbdc165dbe71939b1d2d09a74d442
RenataNovais <rnovaiscs@gmail.com>
2021-03-27 22:37:48 -0300

    Requisito 3.

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,39 @@
+import csv
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    """Importa um arquivo CSV e retorna os dados como objeto."""
+
+    ext = filepath[-3:]
+    valid_header = [
+        'url',
+        'title',
+        'timestamp',
+        'writer',
+        'shares_count',
+        'comments_count',
+        'summary',
+        'sources',
+        'categories'
+    ]
+
+    if (ext != 'csv'):
+        raise ValueError('Formato invalido')
+
+    try:
+        with open(filepath) as file:
+            csv_reader = csv.reader(file, delimiter=";")
+            header, *data = csv_reader
+            print(data)
+
+            if header != valid_header:
+                raise ValueError('Cabeçalhos não compativeis')
+
+            data_result = [
+                {header[i]: content[i] for i in range(len(content))}
+                for content in data
+            ]
+
+            return data_result
+
+    except FileNotFoundError:
+        raise ValueError(f'Arquivo {filepath} não encontrado')

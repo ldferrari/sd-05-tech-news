743e8506a81e02c56588d2b80fc5e081dc562b2d
RenataNovais <rnovaiscs@gmail.com>
2021-03-28 00:05:32 -0300

    Requisito 5.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,21 @@
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+from tech_news.database import search_news
 
+
+def search_by_title(title):
+    """Retorna o titulo e url das noticias encontradas."""
+
+    news = []
+    news_by_title = search_news({
+        'title': {
+            '$regex': title,
+            '$options': 'i'
+        }
+    })
+
+    for new in news_by_title:
+        news.append((new['title'], new['url']))
+
+    return news
 
 def search_by_date(date):
     """Seu código deve vir aqui"""

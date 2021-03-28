381c6870e13436280ad1121bb5f370e731c8e44b
Marina Barcelos <barcelos.marina@gmail.com>
2021-03-27 23:51:49 -0300

    req 4, done

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -17,29 +17,30 @@ def csv_exporter(filepath):
 
     news = database.find_news()
 
-    try:
-        assert filepath.endswith(".csv")
-
-        with open(filepath, "w") as file:
-            csv_writer = csv.DictWriter(
-                file, fieldnames=valid_header, delimiter=";"
-            )
-
-            for rows in news:
-                for key in rows:
-                    # print(key, '--------- chaves')
-                    if type(rows[key]) == list:
-                        # print(rows[key], '--- antes')
-                        rows[key] = ",".join(rows[key])
-                        # print(rows[key], '--- depois')
-
-                csv_writer.writeheader()
-                csv_writer.writerows(news)
-                # print(news)
-
-    # mesmo except para os 2 assertion errors (.csv & header)
-    except AssertionError:
+    # try:
+    # assert filepath.endswith(".csv")
+    # /\ tinha feito assim, mas precisei abandonar esse método
+    #    por conta do flake8 (too complex)
+    #
+    if not filepath.endswith(".csv"):
         raise ValueError("Formato invalido")
 
-    except FileNotFoundError:
-        raise ValueError(f"Arquivo {filepath} não encontrado")
+    with open(filepath, "w") as file:
+        csv_writer = csv.DictWriter(
+            file, fieldnames=valid_header, delimiter=";"
+        )
+
+        for rows in news:
+            for key in rows:
+                # print(key, '--------- chaves')
+                if type(rows[key]) == list:
+                    # print(rows[key], '--- antes')
+                    rows[key] = ",".join(rows[key])
+                    # print(rows[key], '--- depois')
+
+        csv_writer.writeheader()
+        csv_writer.writerows(news)
+        # print(news)
+
+    # except AssertionError:
+    #     raise ValueError("Formato invalido")

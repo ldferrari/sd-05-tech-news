a99564e32568bd9ee0788c2253a6f87eeda7f057
Marina Barcelos <barcelos.marina@gmail.com>
2021-03-28 18:58:18 -0300

    req 5

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,23 @@
+from tech_news import database
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = []
+    news_by_title = database.search_news({
+        "title": {
+            "$regex": title,
+            "$options": "i"
+        }
+    })
+
+    if len(news_by_title) == 0:
+        return []
+
+    else:
+        for article in news_by_title:
+            news.append((article['title'], article['url']))
+
+        return news
 
 
 def search_by_date(date):

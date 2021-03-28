ac23359a67a37f019302c8cc914fd285fc2fce8a
RenataNovais <rnovaiscs@gmail.com>
2021-03-27 23:44:21 -0300

    Requisito 4.

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -4,34 +4,36 @@ def csv_importer(filepath):
     """Importa um arquivo CSV e retorna os dados como objeto."""
 
     ext = filepath[-3:]
-    valid_header = [
-        'url',
-        'title',
-        'timestamp',
-        'writer',
-        'shares_count',
-        'comments_count',
-        'summary',
-        'sources',
-        'categories'
-    ]
+    # valid_header = [
+    #     'url',
+    #     'title',
+    #     'timestamp',
+    #     'writer',
+    #     'shares_count',
+    #     'comments_count',
+    #     'summary',
+    #     'sources',
+    #     'categories'
+    # ]
 
     if (ext != 'csv'):
         raise ValueError('Formato invalido')
 
     try:
         with open(filepath) as file:
-            csv_reader = csv.reader(file, delimiter=";")
-            header, *data = csv_reader
-            print(data)
+            # Conteúdo Course - Manipulando arquivos CSV
+            csv_reader = csv.DictReader(file, delimiter=";")
+            data_result = []
 
-            if header != valid_header:
-                raise ValueError('Cabeçalhos não compativeis')
+            # Opcional, teste não pede essa verificação.
+            # Coloca-lá gerou um aumento de complexidade que
+            # o Flake8 não permite.
+            #
+            # if csv_reader.fieldnames != valid_header:
+            #     raise ValueError('Cabeçalhos não compativeis')
 
-            data_result = [
-                {header[i]: content[i] for i in range(len(content))}
-                for content in data
-            ]
+            for row in csv_reader:
+                data_result.append(row)
 
             return data_result
 

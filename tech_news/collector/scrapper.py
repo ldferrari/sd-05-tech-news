12c46b880abec014e3bd7ff06bce9d696b612aa0
Rafael Assad <rafaassadeng@gmail.com>
2021-03-28 17:32:39 -0300

    req 1git add .

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,14 +1,16 @@
+import requests
+from time import sleep
+
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
     try:
         response = requests.get(url, timeout=timeout)
-    except requests.Timeout:
-                return ''
+    except (requests.ReadTimeout, requests.HTTPError):
+        return ""
     else:
-        if(response.status_code != 200):
-                return ''
-            time.sleeep(delay)
-            return response.text
+        return response.text
+    finally:
+        sleep(delay)
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""

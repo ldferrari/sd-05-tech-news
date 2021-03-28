7361eb1a1bd04c63c70a7ab353557d74a5a776c7
AndersonAGodoy <anderson.a.godoy@gmail.com>
2021-03-28 20:20:03 -0300

    requitos 7 e 8 feitos

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -20,12 +20,27 @@ def search_by_date(date):
     else:
         if len(news) == 0:
             return news
-    return [news[0]["title"], news[0]["url"]]
+    return [(news[0]["title"], news[0]["url"])]
 
 
 def search_by_source(source):
     """Seu código deve vir aqui"""
+    news = database.search_news(
+        {"sources": {"$regex": source, "$options": "-i"}}
+    )
+
+    if len(news) == 0:
+        return news
+
+    return [(news[0]["title"], news[0]["url"])]
 
 
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    news = database.search_news(
+        {"categories": {"regex": category, "$options": "-i"}}
+    )
+
+    if len(news) == 0:
+        return news
+    return [(news[0]["title"], news[0]["url"])]

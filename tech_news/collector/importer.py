bfce8df82e265cece4158209f65acb3bb2d8a27a
Marina Barcelos <barcelos.marina@gmail.com>
2021-03-27 22:38:42 -0300

    req 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,41 @@
+import csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    valid_header = [
+        "url",
+        "title",
+        "timestamp",
+        "writer",
+        "shares_count",
+        "comments_count",
+        "summary",
+        "sources",
+        "categories",
+    ]
+
+    try:
+        assert filepath.endswith(".csv")
+
+        with open(filepath) as file:
+            csv_reader = csv.reader(file, delimiter=";")
+            # separa o header do restante (referência: conteúdo)
+            header, *data = csv_reader
+
+            assert header == valid_header
+
+            result = [
+                {header[i]: content[i] for i in range(len(content))}
+                for content in data
+            ]
+
+            return result
+
+    # mesmo except para os 2 assertion errors (.csv & header)
+    except AssertionError:
+        raise ValueError("Formato invalido")
+
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")
+
+# https://docs.python.org/3/library/exceptions.html#ValueError

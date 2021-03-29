700b4e3f0fffdf0ddd38e2d68e8aa4347b3df80e
Rodrigo Freitas <rfreitasbatista@gmail.com>
2021-03-29 10:37:57 -0300

    lint2

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -4,7 +4,6 @@ from tech_news import database
 
 def csv_exporter(filepath):
 
-
     if not filepath.endswith(".csv"):
         raise ValueError("Formato invalido")
     headers = [
@@ -20,9 +19,7 @@ def csv_exporter(filepath):
     ]
 
     with open(filepath, "w") as file:
-        csv_writer = csv.DictWriter(
-            file, fieldnames=headers, delimiter=";"
-        )
+        csv_writer = csv.DictWriter(file, fieldnames=headers, delimiter=";")
 
         news_db = database.find_news()
         for new in news_db:

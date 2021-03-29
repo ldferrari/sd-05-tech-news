09d974d13382ba5bcb6dc76881b209ca2cbe2c5c
Christian Duessel <duessel@gmail.com>
2021-03-29 14:58:37 -0300

    req9 start

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,17 +1,30 @@
-from tech_news.database import find_news
+# from tech_news.database import find_news
 
 
-def top_5_news():
-    news = find_news()
-    for art in news:
-        art["total"] = (f'${art["shares_count"]+art["comments_count"]}')
-    sorted = news.sort("total", reverse=True)
-    limited = sorted[:5]
-    resultado = []
-    for noticia in limited:
-        resultado.append((noticia['title'], noticia['url']))
-    return resultado
+# def top_5_news():
+#     news = find_news()
+#     totalizer = []
+#     for art in news:
+#         totalizer.append(
+#             {
+#                 'title': art['title'],
+#                 'url': art['url'],
+#                 'total': (art['comments_count']+art['shares_count'])},
+#         )
+#         # totalizer.append({'total': (art['comments_count']+art['shares_count'])})
 
+#     print(totalizer)
+#     def myFunc(e):
+#         return e['total']
+#     sorted = totalizer.sort(key=myFunc)
+#     # https://www.w3schools.com/python/ref_list_sort.asp
+#     # limited = sorted[:5]
+#     print(sorted)
+#     resultado = []
+#     for noticia in sorted:
+#         resultado.append((noticia['title'], noticia['url']))
+    
+#     return resultado
 
-def top_5_categories():
-    """Seu código deve vir aqui"""
+# def top_5_categories():
+#     """Seu código deve vir aqui"""

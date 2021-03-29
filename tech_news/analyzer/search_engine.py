3f604d7f26d1b246c274f9beaab106ace70ee082
hleoc <informig@gmail.com>
2021-03-28 23:20:37 -0300

    Sétimo requisito

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -26,7 +26,11 @@ def search_by_date(date):
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    search = search_news({"sources": {"$regex": source, "$options": "i"}})
+    if search == []:
+        return []
+    news = [(search[0]["title"], search[0]["url"])]
+    return news
 
 
 def search_by_category(category):

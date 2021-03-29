c39294cdd46c632ebd19756f560e67c4eed2c664
Natalia Macedo <nataliasilvamacedo@gmail.com>
2021-03-29 11:14:18 -0300

    Requisito 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,19 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+
+        if response.status_code != 200:
+            return ""
+
+        return response.text
+
+    except requests.Timeout:
+        return ""
 
 
 def scrape(fetcher, pages=1):

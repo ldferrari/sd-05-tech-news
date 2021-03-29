91d65e2e1f371bb00925bb5d51265523699ade31
pauloricardoz <trybe.przt@gmail.com>
2021-03-29 11:54:56 -0300

    search by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,27 @@
+from datetime import datetime
 from tech_news.database import search_news
 
 
 # search_news(query): return list(db.news.find(query))
 def search_by_title(title):
-    # Method The title() method returns a string 
+    # Method The title() method returns a string
     # where the first character in every word is upper case.
     busca = search_news({"title": title.title()})
     if len(busca) == 0:
         return []
-    return [(busca[0]['title'], busca[0]['url'])]
+    return [(busca[0]["title"], busca[0]["url"])]
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        busca = search_news({"timestamp": {"$regex": date}})
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        if len(busca) == 0:
+            return []
+        return [(busca[0]["title"], busca[0]["url"])]
 
 
 def search_by_source(source):

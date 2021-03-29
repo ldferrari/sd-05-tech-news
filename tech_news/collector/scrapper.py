c89827927df8c494e6aea61632bf7b8e4fd15f42
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2021-03-29 18:06:06 -0300

    item 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,16 @@
+import requests
+from time import sleep
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+    except requests.Timeout:
+        return ""
+    else:
+        if (response.status_code != 200):
+            return ""
+        sleep(delay)
+        return response.text
 
 
 def scrape(fetcher, pages=1):

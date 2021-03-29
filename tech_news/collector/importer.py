e77740fd2c94a1e4a387ad4ef480f9177a20842f
Natalia Macedo <nataliasilvamacedo@gmail.com>
2021-03-29 12:08:36 -0300

    Requisito 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,17 @@
+import csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+
+    data = []
+    try:
+        with open(filepath) as file:
+            read_file = csv.DictReader(file, delimiter=";")
+            for item in read_file:
+                data.append(item)
+            return data
+
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")
--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -17,4 +17,3 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""

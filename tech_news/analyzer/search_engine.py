579caecb2055dc045c030b04084e5dcb3d3fae69
Rodrigo Freitas <rfreitasbatista@gmail.com>
2021-03-29 10:35:50 -0300

    lint

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -13,14 +13,11 @@ def search_by_title(title):
         news.append((new["title"], new["url"]))
     return news
 
-
 def search_by_date(date):
     """Seu código deve vir aqui"""
 
-
 def search_by_source(source):
     """Seu código deve vir aqui"""
-
 
 def search_by_category(category):
     """Seu código deve vir aqui"""

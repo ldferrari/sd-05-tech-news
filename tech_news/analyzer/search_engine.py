ede5566de6f981a7ca6acb2e8124f4786c5f9e5d
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2021-03-30 17:06:48 -0300

    item 5 e 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,37 @@
+from tech_news import database
+from datetime import datetime
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    #   ref1
+    noticias = []
+    pesquisa_por_titulo = database.search_news({
+        "title": {
+            "$regex": title,
+            "$options": "i",
+        }
+    })
+
+    for item in pesquisa_por_titulo:
+        noticias.append((item["title"], item["url"]))
+    return noticias
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    #   2020-11-23 - exemplo
+    #   ref2:
+    noticias = []
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        noticias = database.search_news({
+            "timestamp": {
+                "$regex": date
+            }
+        })
+    return [(item["title"], item["url"]) for item in noticias]
 
 
 def search_by_source(source):
@@ -12,3 +40,6 @@ def search_by_source(source):
 
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+#   ref1: https://docs.mongodb.com/manual/reference/operator/query/regex/
+#   ref2: https://www.programiz.com/python-programming/datetime/strptime

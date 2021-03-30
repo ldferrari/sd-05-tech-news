cd369b6be7a449e654a2161d861b9e3817fd624d
llsxavier <leonardoxw@gmail.com>
2021-03-30 08:54:15 -0300

    pymongo <3

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,20 @@
+import pymongo
+from tech_news.database import db
+
+
 def top_5_news():
     """Seu código deve vir aqui"""
+    top_5_news = []
+    news = (db.news.find({}).sort([
+        ('shares_count', pymongo.DESCENDING),
+        ('comments_count', pymongo.DESCENDING),
+        ('title', pymongo.ASCENDING)
+    ]).limit(5))
+
+    for new in news:
+        top_5_news.append((new['title'], new['url']))
+
+    return top_5_news
 
 
 def top_5_categories():

79cbd3e22509ea40de16911e2008710a2ce2471d
llsxavier <leonardoxw@gmail.com>
2021-03-30 09:35:15 -0300

    mais facil pesquisar "son" do que resolver o req2

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,6 @@
 import pymongo
 from tech_news.database import db
+from bson.son import SON
 
 
 def top_5_news():
@@ -19,3 +20,15 @@ def top_5_news():
 
 def top_5_categories():
     """Seu código deve vir aqui"""
+    top_5_news = []
+    news = db.news.aggregate([
+        {"$unwind": "$categories"},
+        {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+        {"$sort": SON([("count", 1), ("_id", 1)])},
+        {"$limit": 5},
+    ])
+
+    for new in news:
+        top_5_news.append((new["_id"]))
+
+    return top_5_news
